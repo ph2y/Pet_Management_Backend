c5496db3a9ee8b668885df9928a6e786ba7b8273
Komputer-P <wlghks0314@gmail.com>
2022-01-22 15:27:21 +0900

    email sender changed, template changed

--- src/main/java/com/sju18/petmanagement/global/email/EmailService.java
+++ src/main/java/com/sju18/petmanagement/global/email/EmailService.java
@@ -26,19 +26,19 @@ public class EmailService {
         MimeMessage  message = emailSender.createMimeMessage();
 
         message.addRecipients(Message.RecipientType.TO, to); //보내는 대상
-        message.setSubject("Pet Management 서비스 인증 코드: " + currentAuthCode); //제목
+        message.setSubject("집사의 노트 서비스 인증 코드: " + currentAuthCode); //제목
 
         String verificationMessage="";
         verificationMessage += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">이메일 주소 확인</h1>";
         verificationMessage += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 확인 코드를 인증코드란에 입력하십시오.</p><br/>";
-        verificationMessage += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">본 확인 코드는 10분간만 유효합니다.</p>";
-        verificationMessage += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">확인 코드는 Pet-Management 서비스의 회원가입, 비밀번호 찾기 등에 사용되므로, 만약, 본인이 요청한 것이 아니라면 본 이메일을 무시하십시오.</p><br/>";
         verificationMessage += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
         verificationMessage += currentAuthCode;
         verificationMessage += "</td></tr></tbody></table></div>";
+        verificationMessage += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">본 확인 코드는 10분간만 유효합니다.</p>";
+        verificationMessage += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">확인 코드는 집사의 노트 서비스의 회원가입, 비밀번호 찾기 등에 사용되므로, 만약, 본인이 요청한 것이 아니라면 본 이메일을 무시하십시오.</p><br/>";
 
         message.setText(verificationMessage, "utf-8", "html"); //내용
-        message.setFrom(new InternetAddress("smyun99@gmail.com","pet-management")); //보내는 사람
+        message.setFrom(new InternetAddress("ph2ydev@gmail.com","집사의 노트")); //보내는 사람
 
         return message;
     }
@@ -64,7 +64,7 @@ public class EmailService {
         MimeMessage  message = emailSender.createMimeMessage();
 
         message.addRecipients(Message.RecipientType.TO, to); //보내는 대상
-        message.setSubject("Pet Management 비밀번호 찾기: 임시 비밀번호 통지"); //제목
+        message.setSubject("집사의 노트 비밀번호 찾기: 임시 비밀번호 통지"); //제목
 
         String tempPasswordNotifyMessage="";
         tempPasswordNotifyMessage += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">임시 비밀번호 통지</h1>";
@@ -74,7 +74,7 @@ public class EmailService {
         tempPasswordNotifyMessage += "</td></tr></tbody></table></div>";
 
         message.setText(tempPasswordNotifyMessage, "utf-8", "html"); //내용
-        message.setFrom(new InternetAddress("smyun99@gmail.com","pet-management")); //보내는 사람
+        message.setFrom(new InternetAddress("ph2ydev@gmail.com","집사의 노트")); //보내는 사람
 
         return message;
     }
