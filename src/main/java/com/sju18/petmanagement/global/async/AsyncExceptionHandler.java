aa8fe4b54d4b676a98cb8d785bf20e5e518de202
Komputer-P <wlghks0314@gmail.com>
2022-03-05 17:25:57 +0900

    async implemented in notificationPushService

---
+++ src/main/java/com/sju18/petmanagement/global/async/AsyncExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.sju18.petmanagement.global.async;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+
+import java.lang.reflect.Method;
+
+public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+    @Override
+    public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+        System.out.println("--- THREAD ERROR ---");
+        System.out.println("Exception Message: " + ex.getMessage());
+        System.out.println("Exception Method: " + method.getName());
+        for(Object param : params) {
+            System.out.println("Exception Parameter Values: " + param);
+        }
+    }
+}
