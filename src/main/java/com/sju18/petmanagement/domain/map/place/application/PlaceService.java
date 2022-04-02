d291c64c5a194bc0cbd5f91e8b5f6f68c8b87c8a
Komputer-P <wlghks0314@gmail.com>
2022-04-02 15:56:55 +0900

    map search with random sampling implemented

--- src/main/java/com/sju18/petmanagement/domain/map/place/application/PlaceService.java
+++ src/main/java/com/sju18/petmanagement/domain/map/place/application/PlaceService.java
@@ -30,6 +30,12 @@ public class PlaceService {
     public static final int NO_CHANGE = 0;
     public static final int DECREMENT = -1;
 
+    public static final double DISTRICT_SCALE = 10000.0;
+    public static final double CITY_SCALE = 30000.0;
+
+    public static final double CITY_SCALE_SAMPLE_SIZE = 0.3;
+    public static final double METROPOLITAN_SCALE_SAMPLE_SIZE = 0.1;
+
     // CREATE
     @Transactional
     public void createPlace(CreatePlaceReqDto reqDto) {
@@ -62,18 +68,47 @@ public class PlaceService {
     @Transactional(readOnly = true)
     public List<Place> fetchPlaceByKeywordAndDistance(FetchPlaceReqDto reqDto) {
         List<Double> distanceRange = calDistanceRange(reqDto);
+        double searchRange = reqDto.getRange().doubleValue();
 
-        if(EnumUtils.isValidEnum(CategoryCode.class, reqDto.getKeyword())) {
-            return placeRepository.fetchAllByCategoryCodeAndDistance(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword());
+        if(searchRange <= DISTRICT_SCALE) {
+            if(EnumUtils.isValidEnum(CategoryCode.class, reqDto.getKeyword())) {
+                return placeRepository.findAllByCategoryCodeAndDistance(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword());
+            }
+            else {
+                return placeRepository.findAllByKeywordAndDistance(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword());
+            }
+        }
+        else if(DISTRICT_SCALE < searchRange && searchRange <= CITY_SCALE) {
+            if(EnumUtils.isValidEnum(CategoryCode.class, reqDto.getKeyword())) {
+                return placeRepository.findAllByCategoryCodeAndDistanceWithRandomSampling(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword(), CITY_SCALE_SAMPLE_SIZE);
+            }
+            else {
+                return placeRepository.findAllByKeywordAndDistanceWithRandomSampling(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword(), CITY_SCALE_SAMPLE_SIZE);
+            }
         }
         else {
-            return placeRepository.fetchAllByKeywordAndDistance(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword());
+            if(EnumUtils.isValidEnum(CategoryCode.class, reqDto.getKeyword())) {
+                return placeRepository.findAllByCategoryCodeAndDistanceWithRandomSampling(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword(), METROPOLITAN_SCALE_SAMPLE_SIZE);
+            }
+            else {
+                return placeRepository.findAllByKeywordAndDistanceWithRandomSampling(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), reqDto.getKeyword(), METROPOLITAN_SCALE_SAMPLE_SIZE);
+            }
         }
     }
     @Transactional(readOnly = true)
     public List<Place> fetchPlaceByDistance(FetchPlaceReqDto reqDto) {
         List<Double> distanceRange = calDistanceRange(reqDto);
-        return placeRepository.findAllByDistance(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3));
+        double searchRange = reqDto.getRange().doubleValue();
+
+        if(searchRange <= DISTRICT_SCALE) {
+            return placeRepository.findAllByDistance(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3));
+        }
+        else if(DISTRICT_SCALE < searchRange && searchRange <= CITY_SCALE) {
+            return placeRepository.findAllByDistanceWithRandomSampling(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), CITY_SCALE_SAMPLE_SIZE);
+        }
+        else {
+            return placeRepository.findAllByDistanceWithRandomSampling(distanceRange.get(0), distanceRange.get(1), distanceRange.get(2), distanceRange.get(3), METROPOLITAN_SCALE_SAMPLE_SIZE);
+        }
     }
 
     private List<Double> calDistanceRange(FetchPlaceReqDto reqDto) {
--- src/main/java/com/sju18/petmanagement/domain/map/place/dao/PlaceRepository.java
+++ src/main/java/com/sju18/petmanagement/domain/map/place/dao/PlaceRepository.java
@@ -25,7 +25,7 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
             value = "SELECT * FROM place AS p WHERE ((:latMin <= p.latitude AND p.latitude <= :latMax) AND (:longMin <= p.longitude AND p.longitude <= :longMax)) AND (p.category_code = :categoryCode)",
             nativeQuery = true
     )
-    List<Place> fetchAllByCategoryCodeAndDistance(
+    List<Place> findAllByCategoryCodeAndDistance(
             @Param("latMin") Double latMin,
             @Param("latMax") Double latMax,
             @Param("longMin") Double longMin,
@@ -36,11 +36,46 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
             value = "SELECT * FROM place AS p WHERE ((:latMin <= p.latitude AND p.latitude <= :latMax) AND (:longMin <= p.longitude AND p.longitude <= :longMax)) AND (p.name LIKE %:keyword%)",
             nativeQuery = true
     )
-    List<Place> fetchAllByKeywordAndDistance(
+    List<Place> findAllByKeywordAndDistance(
             @Param("latMin") Double latMin,
             @Param("latMax") Double latMax,
             @Param("longMin") Double longMin,
             @Param("longMax") Double longMax,
             @Param("keyword") String keyword
+    );
+    @Query(
+            value = "SELECT * FROM place AS p WHERE (:latMin <= p.latitude AND p.latitude <= :latMax) AND (:longMin <= p.longitude AND p.longitude <= :longMax) AND RAND() <= :sampleSize",
+            nativeQuery = true
+    )
+    List<Place> findAllByDistanceWithRandomSampling(
+            @Param("latMin") Double latMin,
+            @Param("latMax") Double latMax,
+            @Param("longMin") Double longMin,
+            @Param("longMax") Double longMax,
+            @Param("sampleSize") Double sampleSize
+    );
+    @Query(
+            value = "SELECT * FROM place AS p WHERE ((:latMin <= p.latitude AND p.latitude <= :latMax) AND (:longMin <= p.longitude AND p.longitude <= :longMax)) AND (p.category_code = :categoryCode)  AND RAND() <= :sampleSize",
+            nativeQuery = true
+    )
+    List<Place> findAllByCategoryCodeAndDistanceWithRandomSampling(
+            @Param("latMin") Double latMin,
+            @Param("latMax") Double latMax,
+            @Param("longMin") Double longMin,
+            @Param("longMax") Double longMax,
+            @Param("categoryCode") String categoryCode,
+            @Param("sampleSize") Double sampleSize
+    );
+    @Query(
+            value = "SELECT * FROM place AS p WHERE ((:latMin <= p.latitude AND p.latitude <= :latMax) AND (:longMin <= p.longitude AND p.longitude <= :longMax)) AND (p.name LIKE %:keyword%) AND RAND() <= :sampleSize",
+            nativeQuery = true
+    )
+    List<Place> findAllByKeywordAndDistanceWithRandomSampling(
+            @Param("latMin") Double latMin,
+            @Param("latMax") Double latMax,
+            @Param("longMin") Double longMin,
+            @Param("longMax") Double longMax,
+            @Param("keyword") String keyword,
+            @Param("sampleSize") Double sampleSize
     );
 }
